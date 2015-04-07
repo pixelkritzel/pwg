514ae17f03428cb2e8a5e43a872bf3c98c4f738e
Timo Zoeller <timo@imazine.de>
2015-04-07 19:59:43 +0200

    Ember-CLI Update

--- app/router.js
+++ app/router.js
@@ -5,9 +5,7 @@ var Router = Ember.Router.extend({
   location: config.locationType
 });
 
-Router.map(function() {
+export default Router.map(function() {
   this.route('pwg', { path: '/' });
   this.route('services');
 });
-
-export default Router;
